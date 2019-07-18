83f299690e45387550c562f46ae37d6f19743c57
Krzysztof Furtak <kfurtak@najlepszefoto.pl>
2019-07-18 13:54:56 +0200

    -

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,35 @@
 import Canvas from './Canvas.js';
 
+const checkInputActiveState = (input) => {
+    if (input.value) {
+        input.parentElement.classList.add('active');
+        return;
+    } 
+
+    input.parentElement.classList.remove('active');
+};
+
+const checkInputFocusState = (input) => {
+    input.addEventListener('focus', function() {
+        this.parentElement.classList.add('focused');
+    });
+
+    input.addEventListener('blur', function() {
+        this.parentElement.classList.remove('focused');
+    });
+};
+
 window.addEventListener('load', () => {
     new Canvas('#bg-anim', '.header');
+
+    const inputs = [...document.querySelectorAll('.form input')];
+
+    inputs.map(input => {
+        checkInputFocusState(input);
+        checkInputActiveState(input);
+
+        input.addEventListener('keyup', function() {
+            checkInputActiveState(this);
+        });
+    });
 });
